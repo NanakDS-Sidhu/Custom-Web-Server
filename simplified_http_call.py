3e594b854e5b9990094c2626b361d7bc5d2ffbe7
NanakDS-Sidhu <sidhu.nds22@gmail.com>
2026-01-03 17:49:42 +0530

    added a simplified HTTP call

---
+++ simplified_http_call.py
@@ -0,0 +1,13 @@
+import socket 
+
+#create a connection to port
+s=socket.create_connection(("127.0.0.1",8888))
+
+#send a get request to the server
+s.sendall(b"GET / HTTP/1.1\r\nHOST:localhost\r\n\r\n")
+
+#recieve upto 1024 bits of the response
+print(s.recv(1024).decode())
+s.close()
+
+#use to test the server 
