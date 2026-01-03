72a7e973041c7a2195df26a4d519aef7a9564572
NanakDS-Sidhu <sidhu.nds22@gmail.com>
2026-01-04 01:49:49 +0530

    changed app config to work with custom data

---
+++ app.py
@@ -0,0 +1,24 @@
+from custom_app import App,Response
+import asyncio
+from HTTP_server import HTTPServer
+
+app = App()
+
+@app.route("/", methods=["GET"])
+async def home(request):
+    return Response("<h1 style='background:blue;'>Welcome Home</h1>", headers={"content-type": "text/html"})
+
+@app.route("/echo", methods=["POST"])
+async def echo(request):
+    data = await request.body()
+    return Response(f"You posted: {data.decode()}")
+
+async def main():
+    server_logic = HTTPServer(app)
+    server = await asyncio.start_server(server_logic.handle_client, "127.0.0.1", 8000)
+    print("Full-Stack Custom Server running on http://127.0.0.1:8000")
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
